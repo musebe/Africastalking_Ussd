e18ac083978646e6ddc9688d58a378c8aae4193d
musebe <emusebe9@gmail.com>
2019-02-28 16:17:10 +0000

    Added sample flow

---
+++ flow.js
@@ -0,0 +1,36 @@
+[
+  {
+    questionText: "What is your name",
+    category: "sample",
+    options: [
+      {
+        questionId: "1202-1232-ddsd-fdf",
+        optionText: "Option 1",
+        weight: "",
+        publicId: "2023203-3232-ewsss"
+      }
+    ],
+    publicId: "ds-dsdfs-fdfdf",
+    testTypeId: "sdsdfs-dfdf-fdfd"
+  },
+  {
+    questionText: "What is your other name",
+    category: "sample",
+    options: [
+      {
+        questionId: "1202-1232-ddsd-fdf",
+        optionText: "Option 1",
+        weight: "",
+        publicId: "2023203-3232-ewsss"
+      },
+      {
+        questionId: "1202-1232-ddsd-fdf",
+        optionText: "Option 2",
+        weight: "",
+        publicId: "2023203-3232-ewsss"
+      }
+    ],
+    publicId: "ds-dsdfs-fdfdf",
+    testTypeId: "sdsdfs-dfdf-fdfd"
+  }
+];
